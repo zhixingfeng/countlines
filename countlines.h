93f4d32e1b45f5d097635668cf3420124bef13c0
Zhixing Feng <zhixingfeng@zhixingdemacbook-pro.local>
2017-03-19 18:57:28 -0400

    merge count files

--- countlines.h
+++ countlines.h
@@ -16,7 +16,9 @@
 #include <cstdint>
 #include <sstream>
 #include <stdexcept>
-
+#include <cstdint>
+//#include <cstdlib>
+#include <stdlib.h>
 using namespace std;
 
 inline int64_t wc_l(string filename);
@@ -24,6 +26,7 @@ inline vector<string> getsplitfiles(string dir);
 inline vector<string> getcountfiles(string dir);
 inline bool checksort(string filename);
 inline vector<string> countlines_split(const string &s, char delim, bool rm_empty=true);
+inline void countlines_merge(string infile, string outfile);
 
 inline void countlines(string input_file, string output_file, int n_split, string tmpdir)
 {
@@ -63,7 +66,13 @@ inline void countlines(string input_file, string output_file, int n_split, strin
     vector<string> countfiles = getcountfiles(tmpdir + "/tmp_countlines");
     checksort(countfiles[0]);
     
-    // merge count files
+    // merge count files by sort -m 
+    shell_cmd = "sort -m -k2,2 " + tmpdir + "/tmp_countlines/splitfile*.count > " + tmpdir + "/tmp_countlines/merge.count";
+    cout << shell_cmd << endl;
+    system(shell_cmd.c_str());
+    
+    // merge counts of merge.count file
+    countlines_merge(tmpdir + "/tmp_countlines/merge.count",  output_file);
     
     
 }
@@ -135,7 +144,7 @@ inline bool checksort(string filename)
     ifstream fs_file(filename);
     if (!fs_file.is_open())
         throw runtime_error("fail to open " + filename);
-    int n = 0;
+    int64_t n = 0;
     string last_line = "!";
     while(1){
         string buf;
@@ -168,5 +177,43 @@ inline vector<string> countlines_split(const string &s, char delim, bool rm_empt
     return tokens;
 }
 
+inline void countlines_merge(string infile, string outfile)
+{
+    ifstream fs_infile(infile);
+    if (!fs_infile.is_open())
+        throw runtime_error("fail to open " + infile);
+    ofstream fs_outfile(outfile);
+    if (!fs_outfile.is_open())
+        throw runtime_error("fail to open " + outfile);
+    string last_line = "!";
+    int64_t last_count = 0;
+    int64_t n = 0;
+    while (1){
+        string buf;
+        getline(fs_infile, buf);
+        if (fs_infile.eof())
+            break;
+        n++;
+        vector<string> buf_vec = countlines_split(buf, ' ', true);
+        if (buf_vec.size()!=2)
+            throw runtime_error("incorrect format at line " + to_string(n));
+        if (buf_vec[1] < last_line)
+            throw runtime_error("incorrect order at line " + to_string(n));
+        string cur_line = buf_vec[1];
+        int64_t cur_count = stoll(buf_vec[0]);
+        if (cur_line == last_line){
+            last_count += cur_count;
+        }else{
+            if (last_line > "!")
+                fs_outfile << last_count << '\t' << last_line << endl;
+            last_count = cur_count;
+            last_line = cur_line;
+        }
+        
+    }
+    
+    fs_infile.close();
+    fs_outfile.close();
+}
 
 #endif /* countlines_h */
